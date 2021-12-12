65350600fd30a5a783bad938d9cd0066e4b811a9
Pramono Utomo <emailbuatcariduit@gmail.com>
2021-12-12 08:29:19 +0700

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,23 @@
+#Generate config for node
+config_node_file=/Paw"/config-node.toml"
+ip=$(curl -s https://ipinfo.io/ip)
+if [ ! -f $config_node_file ]
+then
+    echo "Creating node config" $config_node_file
+    node_config=$(paw_node --generate_config node)
+    node_config=$(echo "$node_config" | sed "s/\[rpc\]/[rpc]\n\nenable = true/g")
+    node_config=$(echo "$node_config" | sed "s/\#external_address\ \= \"\:\:\"/external_address = \"::ffff:${ip}\"/g")
+    node_config=$(echo "$node_config" | sed "s/\#external_port\ \= 0/external_port = 7045/g")
+    node_config=$(echo "$node_config" | sed "s/\#enable_voting\ \=\ false/enable_voting = true/g")
+    echo "$node_config" > $config_node_file
+fi
+
+#Generate config for rpc
+rpc_node_file=/Paw"/config-rpc.toml"
+if [ ! -f $rpc_node_file ]
+then
+    echo "Creating rpc config" $rpc_node_file
+    rpc_config=$(paw_node --generate_config rpc)
+    rpc_config=$(echo "$rpc_config" | sed "s/\#enable_control\ \=\ false/enable_control = true/g")
+    echo "$rpc_config" > $rpc_node_file
+fi
